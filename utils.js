0abb143c43041f3a3b7965b69ef0d51bd44be42e
Mimi <1119186082@qq.com>
2021-06-13 17:02:10 +0800

    Remove luxon

--- utils.js
+++ utils.js
@@ -1,9 +1,4 @@
 import cheerio from "cheerio";
-import { DateTime, Settings } from "luxon";
-
-function parseCoord(ele, index) {
-    return parseFloat(getText(ele, index));
-}
 
 class DataBase {
     constructor(tracklog) {
@@ -28,40 +23,20 @@ class DataBase {
             speed,
             latitude: [],
             longitude: [],
-            heading: []
+            heading: [],
+            length: 0
         };
-
-        Settings.defaultZoneName = "utc";
-        const ref = DateTime.fromMillis(time[0]).startOf("day");
-        let dayOffset = 0;
-        let currDay;
 
         const $ = cheerio.load(tracklog);
         $(".smallrow1, .smallrow2").each((index, ele) => {
-            if ($(ele).is(".flight_event_facility, .flight_event")) {
+            if (this.data.length > time.length || $(ele).is(".flight_event_facility, .flight_event")) {
                 return;
             }
             const children = $(ele).children();
-            const timeString = this.getText(children, 0);
-            let day = timeString[2];
-            const t = timeString.slice(4);
-            if (!currDay) {
-                currDay = day;
-            } else if (currDay !== day) {
-                currDay = day;
-                dayOffset++;
-            }
-            day = ref.plus({ days: dayOffset }).toISODate();
-            const ms = DateTime.fromSQL(`${day} ${t}`).toMillis();
-
-            if (time.includes(ms)) {
-                this.data.latitude.push(parseFloat(this.getText(children, 1)));
-                this.data.longitude.push(parseFloat(this.getText(children, 2)));
-                this.data.heading.push(parseInt(children.eq(3).text().match(/\d+/)[0]));
-            } else {
-                //console.warn("Time not match!", day, time);
-            }
-            //console.log($(ele).html());
+            this.data.latitude.push(parseFloat(this.getText(children, 1)));
+            this.data.longitude.push(parseFloat(this.getText(children, 2)));
+            this.data.heading.push(parseInt(children.eq(3).text().match(/\d+/)[0]));
+            this.data.length++;
         });
     }
 
