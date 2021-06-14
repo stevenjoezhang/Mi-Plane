27cf1a9275016797b5a81565d17a756c508d705c
Mimi <1119186082@qq.com>
2021-06-15 02:00:23 +0800

    New loading style

---
+++ src/loading.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import "./loading.css";
+
+class Loading extends Component {
+    render() {
+        return <div className="loading">
+            <div className="cube"></div>
+            <div className="cube"></div>
+            <div className="cube"></div>
+            <div className="cube"></div>
+            <div className="cube"></div>
+            <div className="cube"></div>
+            <div className="cube"></div>
+            <div className="cube"></div>
+            <div className="cube"></div>
+        </div>;
+    }
+}
+
+export default Loading;
